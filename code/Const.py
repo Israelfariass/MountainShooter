3ff0578013869f7a0792987401764579c76c0968
israelfarias01 <israelfarias333@hotmail.com>
2025-09-01 15:57:41 -0300

    Creating main loop Connecting Menu end Game classes

---
+++ code/Const.py
@@ -0,0 +1,16 @@
+# C
+COLOR_ORANGE = (255, 128, 0)
+C_WHITE = (255, 255, 255)
+
+# M
+MENU_OPTION = ('NEW GAME1P',
+               'NEW GAME2P - COOPERATIVE',
+               'NEW GAME2P - COOPERATIVE' ,
+               'SCORE',
+               'EXIT')
+
+# W
+WIN_WIDTH = 576
+WIN_HEIGHT: int = 324
+
+
